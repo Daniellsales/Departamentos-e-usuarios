1ffc484d05f7c9bd7bd70487fa046f31ee069068
Daniellsales <danielsales1627@gmail.com>
2023-02-09 15:50:10 -0300

    Criei esse projeto através de uma video aula. canal: DevSuperior

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_department(name) VALUES ('Gestão');
+INSERT INTO tb_department(name) VALUES ('Informática');
+
+INSERT INTO tb_user(id,department_id, name, email) VALUES (1,1, 'Daniel', 'daniel@gmail.com');
+INSERT INTO tb_user(id,department_id, name, email) VALUES (2,1, 'Kamily', 'kamily@gmail.com');
+INSERT INTO tb_user(id,department_id, name, email) VALUES (3,2, 'Alex', 'alex@gmail.com');
+INSERT INTO tb_user(id,department_id, name, email) VALUES (4,2, 'Ana', 'ana@gmail.com');
